a3ca9d31f7b8de2d7efd5e584bd62ba49d2c32b5
TheBlueSoul5188 <hibba04@hotmail.se>
2022-09-30 10:11:48 +0200

    Add project files.

---
+++ uppgift 3.13/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+namespace Uppgift_3._13
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("hur gammal är du? (år) ");
+            double år=double.Parse(Console.ReadLine());
+
+            if (år >= 16 && år <= 19)
+            {
+                Console.WriteLine("du får komma in i tävlingen");
+            }
+            else
+            { 
+                Console.WriteLine("du får inte komma in i tävlingen");
+            }
+
+        }
+    }
+}
+
+
+
+
